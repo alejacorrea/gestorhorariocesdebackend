28f6bf5817371106bb9ee6afa1723a63119c9408
alejacorrea <alejandracp63@gmail.com>
2026-03-13 08:32:42 -0500

    Actualización

--- src/main/java/com/cesde/gestorhorario/Modelo/MPersona.java
+++ src/main/java/com/cesde/gestorhorario/Modelo/MPersona.java
@@ -24,4 +24,55 @@ public class MPersona {
 
     @Column(length = 5)
     Boolean activo;
+
+    public MPersona(String identificacionPersona, String nombrePersona, String correoPersona, String contrasenaPersona, Boolean activo) {
+        this.identificacionPersona = identificacionPersona;
+        this.nombrePersona = nombrePersona;
+        this.correoPersona = correoPersona;
+        this.contrasenaPersona = contrasenaPersona;
+        this.activo = activo;
+    }
+
+    public MPersona() {
+    }
+
+    public String getIdentificacionPersona() {
+        return identificacionPersona;
+    }
+
+    public void setIdentificacionPersona(String identificacionPersona) {
+        this.identificacionPersona = identificacionPersona;
+    }
+
+    public String getNombrePersona() {
+        return nombrePersona;
+    }
+
+    public void setNombrePersona(String nombrePersona) {
+        this.nombrePersona = nombrePersona;
+    }
+
+    public String getCorreoPersona() {
+        return correoPersona;
+    }
+
+    public void setCorreoPersona(String correoPersona) {
+        this.correoPersona = correoPersona;
+    }
+
+    public String getContrasenaPersona() {
+        return contrasenaPersona;
+    }
+
+    public void setContrasenaPersona(String contrasenaPersona) {
+        this.contrasenaPersona = contrasenaPersona;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
