13fe073ceb6931c42e5e8719e1fd2b23c2af9b50
dianabello <leidyaparicio2703@gmail.com>
2026-04-10 10:53:02 -0500

    horarioadmin

---
+++ src/Servicios/SHorarioAdmin.java
@@ -0,0 +1,24 @@
+package com.cesde.gestorhorario.Servicios;
+
+import org.springframework.stereotype.Service;
+
+import com.cesde.gestorhorario.Modelo.MHorarioAdmin;
+
+@Service
+public class SHorarioAdmin {
+    @Autowired
+    IHorarioProfesor iHorarioProfesor;
+
+    public SHorarioAdmin(IHorarioProfesor iHorarioProfesor) {
+        this.iHorarioProfesor = iHorarioProfesor;
+    }
+}
+
+    // Adicion de registros de clientes
+    public MHorarioAdmin adicionarHorario(MHorarioAdmin mHorarioAdmin) throws Exception {
+        try {
+            return iHorarioAdmin.save(mHorarioAdmin);
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
